ab637d8c7623cd4880f1af7a6bce844ee69a2181
Stuart Aitken <s.aitken88@gmail.com>
2020-06-13 12:41:01 +0100

    Embedded images

---
+++ PurePhysicist/PurePhysicist/Helpers/Functions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PurePhysicist.Helpers
+{
+    public static class Functions
+    {
+     
+    }
+}
