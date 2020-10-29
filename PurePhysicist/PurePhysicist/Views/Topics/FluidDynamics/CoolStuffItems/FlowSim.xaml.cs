1536a6514ff64efafca7940f3dc68dd9fb7d4936
Stuart Aitken <s.aitken88@gmail.com>
2020-10-29 23:16:29 +0000

    Complete fluid sim

--- PurePhysicist/PurePhysicist/Views/Topics/FluidDynamics/CoolStuffItems/FlowSim.xaml.cs
+++ PurePhysicist/PurePhysicist/Views/Topics/FluidDynamics/CoolStuffItems/FlowSim.xaml.cs
@@ -17,12 +17,19 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FlowSim : ContentPage
     {
+        public class FluidSimParticle
+        {
+            public int Id { get; set; }
+            public CCDrawNode Node { get; set; }
+            public Particle Particle { get; set; }
+            public double LifeTime { get; set; }
+        }
         #region Private Fields
 
-        private readonly CCDrawNode _centreWheelNode = new CCDrawNode() { Color = CCColor3B.Blue };
-        private readonly List<(int id, CCDrawNode node, Particle particle)> _particles = new List<(int id, CCDrawNode node, Particle charge)>();
-        private readonly int _particlesAmount = 500;
-        private bool _sceneLoaded;
+        private readonly CCDrawNode _centreWheelNode = new CCDrawNode() { Color = CCColor3B.Blue, Visible = false };
+        private readonly List<FluidSimParticle> _particles = new List<FluidSimParticle>();
+        private readonly int _particlesAmount = 650;
+        private readonly int _maxLifeTime = 10;
         private readonly CCLayer _layer = new CCLayer { ContentSize = new CCSize((float)App.DeviceWidth, (float)App.DeviceHeight / 2)};
         private CCSizeI _viewResolution = new CCSizeI(100, 100);
 
@@ -50,7 +57,7 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
         {
             InitializeComponent();
 
-            CommonVelocityFields.FlowStrength = 1;
+            CommonVelocityFields.FlowStrength = 15;
             CommonVelocityFields.CentralObjectRadius = 30;
             CommonVelocityFields.CentralObjectRotation = 5000;
             CommonVelocityFields.FieldDimensions = new Vector(_viewResolution.Width, _viewResolution.Height);
@@ -60,7 +67,6 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
             RotationSlider.Value = CommonVelocityFields.CentralObjectRotation;
 
             FlowPicker.ItemsSource = this.VelocityFields;
-            FlowPicker.SelectedItem = this.VelocityFields.FirstOrDefault(f => f.Field == CommonVelocityFields.UniformStream);
 
             SetupParticles();
 
@@ -77,14 +83,13 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
         {
             base.OnAppearing();
 
-            this._sceneLoaded = true;
-
-            RedrawGameScene();
+            RedrawCentreCircle();
         }
 
-        private void RedrawGameScene()
+        private void RedrawCentreCircle()
         {
-            
+            _centreWheelNode.Clear();
+            _centreWheelNode.DrawSolidCircle(new CCPoint(0, 0), (float) RadiusSlider.Value, CCColor4B.Blue);
         }
 
         #endregion Protected Methods
@@ -100,10 +105,17 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
                 Vector pos = new Vector(rand.Next(0, _viewResolution.Width), rand.Next(0, _viewResolution.Height));
                 var point = new Particle(1, pos);
                 point.ApplyVelocityField(SelectedVelocityField);
-                _particles.Add((i, new CCDrawNode() {Position = new CCPoint((float)pos.X, (float)pos.Y) }, point));
-                _layer.AddChild(_particles[i].node);
-                _particles[i].node.DrawSolidCircle(new CCPoint(0, 0), 0.4f, CCColor4B.White);
+                _particles.Add(new FluidSimParticle{
+                    Id = i,
+                    Node = new CCDrawNode() {Position = new CCPoint((float)pos.X, (float)pos.Y) },
+                    Particle = point,
+                    LifeTime =  rand.Next(0, _maxLifeTime)});
+                _layer.AddChild(_particles[i].Node);
+                _particles[i].Node.DrawSolidCircle(new CCPoint(0, 0), 0.3f, CCColor4B.White);
             }
+
+            _centreWheelNode.Position = new CCPoint(_viewResolution.Width / 2, _viewResolution.Height / 2);
+            _layer.AddChild(_centreWheelNode);
         }
 
         private async void BackButton_Clicked(object sender, EventArgs e)
@@ -125,6 +137,7 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
 
                     case "Radius":
                         CommonVelocityFields.CentralObjectRadius = e.NewValue;
+                        RedrawCentreCircle();
                         break;
 
                     case "Rotation":
@@ -164,7 +177,7 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
         {
             gameScene.AddLayer(_layer);
 
-            RedrawGameScene();
+            RedrawCentreCircle();
 
             _layer.Schedule(Update);
         }
@@ -174,32 +187,44 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
             Random rand = new Random();
             double[] sides = { 1, _viewResolution.Width - 1};
             double[] topBottom = { 1, _viewResolution.Height - 1};
-            foreach (var p in _particles)
+            foreach(var p in _particles)
             {
-                p.particle.Move(timeInSeconds);
+                p.Particle.Move(timeInSeconds);
+                p.LifeTime += timeInSeconds;
 
-                if (p.particle.Position.X < 0 || p.particle.Position.X > _viewResolution.Width || p.particle.Position.Y < 0 || p.particle.Position.Y > _viewResolution.Height
-                    || p.particle.Position.X == double.NaN || p.particle.Position.Y == double.NaN)
+                if (p.Particle.Position.X < 0 || p.Particle.Position.X > _viewResolution.Width || p.Particle.Position.Y < 0 || p.Particle.Position.Y > _viewResolution.Height
+                    || p.Particle.Position.X == double.NaN || p.Particle.Position.Y == double.NaN || p.LifeTime > _maxLifeTime)
                 {
+                    p.LifeTime = rand.Next(0, _maxLifeTime);
 
-                    if(rand.Next(0,2) == 1)
+                    if (this.SelectedVelocityField == CommonVelocityFields.UniformStream || this.SelectedVelocityField == CommonVelocityFields.ShearFlow || this.SelectedVelocityField == CommonVelocityFields.FlowPast_Sphere)
                     {
-                        // Place on top or bottom
-                        double topBottomPos = topBottom[rand.Next(0, 2)]; // random of 0 or _viewRes.H
-
-                        p.particle.Position = new Vector(rand.Next(1, _viewResolution.Width - 1), topBottomPos);
+                        // Rightward motion only so reset points on left
+                        p.Particle.Position = new Vector(0, rand.Next(0, _viewResolution.Height));
                     }
                     else
                     {
-                        // Place on left or right
-                        double sidePos = sides[rand.Next(0, 2)]; // random of 0 or _viewRes.W
+                        //Field requires particles to appear from all sides
 
-                        p.particle.Position = new Vector(sidePos, rand.Next(1, _viewResolution.Height - 1));
+                        if (rand.Next(0, 2) == 1)
+                        {
+                            // Place on top or bottom
+                            double topBottomPos = topBottom[rand.Next(0, 2)]; // random of 0 or _viewRes.H
+
+                            p.Particle.Position = new Vector(rand.Next(1, _viewResolution.Width - 1), topBottomPos);
+                        }
+                        else
+                        {
+                            // Place on left or right
+                            double sidePos = sides[rand.Next(0, 2)]; // random of 0 or _viewRes.W
+
+                            p.Particle.Position = new Vector(sidePos, rand.Next(1, _viewResolution.Height - 1));
+                        }
                     }
-
+                    
                 }
 
-                p.node.Position = new CCPoint((float)p.particle.Position.X, (float)p.particle.Position.Y);
+                p.Node.Position = new CCPoint((float)p.Particle.Position.X, (float)p.Particle.Position.Y);
             }
 
             UpdateLabels();
@@ -209,7 +234,9 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
         {
             Device.BeginInvokeOnMainThread(() =>
             {
-                FlowStrengthLabel.Text = $"Flow Strength: {FlowStrengthSlider.Value.DecimalPoints(2)}";
+                FlowStrengthLabel.Text = $"Flow Strength: {FlowStrengthSlider.Value.DecimalPoints(0)}";
+                RadiusLabel.Text = $"Central Radius: {RadiusSlider.Value.DecimalPoints(0)}";
+                RotationLabel.Text = $"Rotation: {RotationSlider.Value.DecimalPoints(0)}";
             });
         }
 
@@ -220,13 +247,109 @@ namespace PurePhysicist.Views.Topics.FluidDynamics.CoolStuffItems
 
             if (selectedIndex != -1)
             {
-                var field = (picker.ItemsSource[selectedIndex] as VelocityFieldListItem).Field;
+                var field = (picker.ItemsSource[selectedIndex] as VelocityFieldListItem)?.Field;
 
                 foreach(var p in _particles)
                 {
-                    p.particle.ApplyVelocityField(field);
+                    p.Particle.ApplyVelocityField(field);
                 }
+
+                this.SelectedVelocityField = field;
+                this.SetUserControls();
             }
+        }
+
+        /// <summary>
+        /// Sets min anx max values such that there will be no chance of crashing when assigning new min/max values
+        /// </summary>
+        /// <param name="s"></param>
+        private void ResetSlider(Slider s)
+        {
+            s.Minimum = int.MinValue;
+            s.Maximum = int.MaxValue;
+        }
+
+        private void SetUserControls()
+        {
+            Device.BeginInvokeOnMainThread(() =>
+            {
+                bool showRadius = this.SelectedVelocityField == CommonVelocityFields.FlowPast_Sphere || this.SelectedVelocityField == CommonVelocityFields.RotatingCylinder || this.SelectedVelocityField == CommonVelocityFields.RankineVortex;
+                bool showRotation = this.SelectedVelocityField == CommonVelocityFields.RotatingCylinder || this.SelectedVelocityField == CommonVelocityFields.RankineVortex ;
+                
+                RadiusLabel.IsVisible =  showRadius;
+                RadiusSlider.IsVisible = showRadius;
+                RotationLabel.IsVisible = showRotation;
+                RotationSlider.IsVisible = showRotation;
+
+                _centreWheelNode.Visible = showRadius;
+
+                if (this.SelectedVelocityField == CommonVelocityFields.StagnationPoint)
+                {
+                    ResetSlider(FlowStrengthSlider);
+                    FlowStrengthSlider.Minimum = 1;
+                    FlowStrengthSlider.Maximum = 5;
+                    FlowStrengthSlider.Value = 2;
+                }
+                else if (this.SelectedVelocityField == CommonVelocityFields.ShearFlow)
+                {
+                    ResetSlider(FlowStrengthSlider);
+                    FlowStrengthSlider.Minimum = 1;
+                    FlowStrengthSlider.Maximum = 10;
+                    FlowStrengthSlider.Value = 3;
+                }
+                else if(this.SelectedVelocityField == CommonVelocityFields.UniformStream)
+                {
+                    ResetSlider(FlowStrengthSlider);
+                    FlowStrengthSlider.Minimum = 15;
+                    FlowStrengthSlider.Maximum = 30;
+                    FlowStrengthSlider.Value = 15;
+                }
+                else
+                {
+                    ResetSlider(FlowStrengthSlider);
+                    FlowStrengthSlider.Maximum = 30;
+                    FlowStrengthSlider.Minimum = 10;
+                    FlowStrengthSlider.Value = 20;
+                }
+
+                if (this.SelectedVelocityField == CommonVelocityFields.RankineVortex)
+                {
+                    _centreWheelNode.Visible = false;
+
+                    FlowStrengthSlider.Value = 15;
+
+                    ResetSlider(RotationSlider);
+                    RotationSlider.Minimum = -50;
+                    RotationSlider.Maximum = 50;
+                    RotationSlider.Value = -30;
+
+                    ResetSlider(RadiusSlider);
+                    RadiusSlider.Minimum = 0;
+                    RadiusSlider.Maximum = 15;
+                    RadiusSlider.Value = 10;
+                }
+
+                if (this.SelectedVelocityField == CommonVelocityFields.FlowPast_Sphere)
+                {
+                    ResetSlider(RadiusSlider);
+                    RadiusSlider.Minimum = 0;
+                    RadiusSlider.Maximum = 40;
+                    RadiusSlider.Value = 25;
+                }
+
+                if (this.SelectedVelocityField == CommonVelocityFields.RotatingCylinder)
+                {
+                    ResetSlider(RadiusSlider);
+                    RadiusSlider.Minimum = 0;
+                    RadiusSlider.Maximum = 40;
+                    RadiusSlider.Value = 10;
+
+                    ResetSlider(RotationSlider);
+                    RotationSlider.Minimum = -10000;
+                    RotationSlider.Maximum = 10000;
+                    RotationSlider.Value = 5000;
+                }
+            });
         }
 
         #endregion Private Methods
