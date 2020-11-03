0ce49ffb3a7dcf25d5d12e42d3b726b9b4971c36
Stuart Aitken <s.aitken88@gmail.com>
2020-11-03 13:44:05 +0000

    Add search bar to cool stuff list view

--- PurePhysicist/PurePhysicist/Views/Topics/TopicPageTemplates/CoolStuffView.xaml.cs
+++ PurePhysicist/PurePhysicist/Views/Topics/TopicPageTemplates/CoolStuffView.xaml.cs
@@ -3,6 +3,7 @@ using PurePhysicist.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,7 +14,8 @@ namespace PurePhysicist.Views.Topics.TopicPageTemplates
     {
         #region Public Properties
 
-        public ObservableCollection<CoolStuffListItem> ListItems { get; set; }
+        public List<CoolStuffListItem> ListItems { get; set; }
+        public ObservableCollection<CoolStuffListItem> ItemsFiltered { get; set; }
         public string PageTitle { get; set; }
         public bool TappedItem { get; set; }
         public Color ThemeColour { get; set; }
@@ -32,7 +34,9 @@ namespace PurePhysicist.Views.Topics.TopicPageTemplates
         {
             this.PageTitle = topicTitle;
             this.ThemeColour = themeColour;
-            this.ListItems = new ObservableCollection<CoolStuffListItem>(items);
+            this.ListItems = new List<CoolStuffListItem>(items);
+            this.ItemsFiltered = new ObservableCollection<CoolStuffListItem>(items);
+
             this.SingleTopicList = topicTitle != Constants.TopicTitles.CoolStuff;
 
             foreach (var item in this.ListItems)
@@ -60,6 +64,36 @@ namespace PurePhysicist.Views.Topics.TopicPageTemplates
                 await this.Navigation.PushModalAsync(((CoolStuffListItem)e.Item).ModalItem.Value);
                 this.TappedItem = false;
             }
+        }
+
+        private void FilterEquationsList(string query)
+        {
+            query = query.MakeNicerQueryString();
+            char[] splitChars = { ' ', '-', '(', ')' };
+            var queryWords = query.Split(splitChars).Where(s => !string.IsNullOrEmpty(s));
+
+            var filtered = (from item in this.ListItems
+                    let label = item.Label.ToLower().MakeNicerQueryString().Trim()
+                    let labelWords = label.Split(splitChars).Where(s => !string.IsNullOrEmpty(s))
+                    where string.IsNullOrEmpty(query)
+                          || labelWords.Intersect(queryWords).Any()
+                          || labelWords.Any(w => queryWords.Any(q => w.StartsWith(q)))
+                          || label.StartsWith(query)
+                    orderby label.StartsWith(query)
+                        //labelWords.Any(w => queryWords.Any(q => w.StartsWith(q)))
+                        descending
+                    select item)
+                .ToList();
+
+            ItemsFiltered = new ObservableCollection<CoolStuffListItem>(filtered);
+            OnPropertyChanged(nameof(ItemsFiltered));
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string query = ((SearchBar)sender).Text.ToLower().Trim(); //remove apostrophes for lazy searching
+
+            this.FilterEquationsList(query);
         }
 
         #endregion Private Methods
